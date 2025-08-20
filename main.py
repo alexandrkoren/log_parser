fffa337c17f644f148919211b68a40a6bbb279ec
alexandrkoren <alexandr5koren@gmail.com>
2025-08-20 16:27:55 +0300

    Add date filter

--- main.py
+++ main.py
@@ -1,18 +1,31 @@
 import argparse
 import json
 from tabulate import tabulate
+from datetime import datetime
 
 
-def get_data_report(files):
+def parse_line(log_data, date=None):
+	url = log_data.get('url', '')
+	response_time = log_data.get('response_time', 0)
+	if date:
+		log_date = datetime.fromisoformat(log_data['@timestamp']).date()
+		if log_date == date:
+			return url, response_time
+		else:
+			return None, None
+	else:
+		return url, response_time
+
+
+def get_data_report(files, date=None):
 	data = {}
 	for file_path in files:
 		try:
 			with open(file_path, 'r', encoding='utf-8') as log_file:
 				for line in log_file:
 					log_data = json.loads(line)
-					url = log_data.get('url', '')
-					response_time = log_data.get('response_time', 0)
-					if url and response_time:
+					url, response_time = parse_line(log_data, date)
+					if url:
 						if url in data:
 							total_time, count = data[url]
 							data[url] = (total_time + response_time, count + 1)
@@ -32,10 +45,12 @@ def main():
 		help='Пути к лог-файлам (можно несколько)'
 	)
 	parser.add_argument('--report', required=True, help='Тип отчета')
+	parser.add_argument('--date', help='Дата в формате YYYY-MM-DD')
 	args = parser.parse_args()
 
 	if args.report == 'average':
-		data = get_data_report(args.file)
+		date = datetime.strptime(args.date, "%Y-%m-%d").date() if args.date else None
+		data = get_data_report(args.file, date)
 		table = [('', 'handler', 'total', 'avg_response_time')]
 		for url, (total_time, count) in data.items():
 			table.append((url, count, round(total_time / count, 3)))
